4eb3dd5ffae9125ae71aef83222edd23b269b2d6
J Roco <jeroco27@gmail.com>
2016-04-22 22:19:55 +1000

    Initial commit 2015-05-08 single cell analysis script

---
+++ 2015-05-08 Single cell qPCR analysis.R
@@ -0,0 +1,101 @@
+#2015-11-16 Single cell qPCR y1 + Actb. 
+
+#Load data:
+#load("~/Desktop/PhD/Results/2015/Single cell PCR/2015-05-08/2015-05-08 Single cell data.RData")
+
+View(`2015-05-08 Single cell qPCR`)
+
+
+#Process data:
+
+dataA <- `2015-05-08 Single cell qPCR`
+#Subset germline transcripts + cMyc + Apex1 + Actb:
+#Targets: c(1,3,5,8,11,14,17,20)
+dataB <- dataA[c(1,3,5,8,11,14,17,15,9,20),]
+dataC <- dataB[c(1,5,6,2,3,7,4,8,9,10),]
+
+#Subset data from day 3.0:
+ data_3.0dA <- dataC[c(2:59)]
+ data_3.0d  <- as.matrix(data_3.0dA[-c(7,15,19,21,24,26,37,38,40,41,48,49,50,55)])
+
+ rownames(data_3.0d) <- dataC[,1]
+ colnames(data_3.0d) <- c(paste("c0",1:9,sep=""),paste("c",10:42,sep=""),"NTC","Bulk")
+
+tdata_3.0d<-t(data_3.0d)
+
+#Subset data from day 7.0:
+data_7.0d<-as.matrix(dataC[60:103])
+rownames(data_7.0d) <- dataC[,1]
+colnames(data_7.0d) <- c(paste("c0",1:9,sep=""),paste("c",10:42,sep=""),"NTC","Bulk")
+
+tdata_7.0d<-t(data_7.0d)
+
+
+x <- as.matrix(cbind(tdata_3.0d,tdata_7.0d))
+y <- as.matrix(rbind(data_3.0d,data_7.0d))
+
+
+
+#Activate gplots package:
+library(gplots)
+
+
+#Set up heatmap-colors:
+   colorRB1 <- colorRampPalette(c("gray51","yellow","darkviolet","black"))
+
+target_lab <- c(expression(gamma[1]),expression(gamma[3]),expression(gamma["2b"]),
+                expression(gamma["2c"]),expression(epsilon),expression(mu),
+                expression(mu - gamma[1]),"c-Myc","Apex1","Actb")
+
+#Heatmap:
+heatmap.2(y,Rowv=F,Colv=F,dendrogram ="none",scale="none",
+          col=colorRB1,
+          trace="none",colsep=(1:(ncol(y))),rowsep=(1:(nrow(y))),sepcolor="black",
+          srtCol=60,cexCol=1,cexRow=1,sepwidth=c(0.07,0.07),
+          adjRow = c(0,0.5),
+          #labRow=c(target_lab,target_lab),
+          breaks=seq(0,40,0.5),
+          lmat=rbind(c(0,3,3,2,0),
+                     c(0,1,1,0,0),
+                     c(0,1,1,0,0),
+                     c(4,4,4,0,0)),
+          lwid=c(0.5,1,1,1,0.05), lhei=c(0.25,0.7,0.7,0.5),
+          density.info="none",keysize=0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################
+target_lab <- c(expression(gamma[1]),expression(gamma[3]),expression(gamma["2b"]),
+                 expression(gamma["2c"]),expression(epsilon),expression(mu),
+                 expression(mu - gamma[1]),"Actb")
+
+#Heatmap:
+heatmap.2(x,Rowv=F,Colv=F,dendrogram ="none",scale="none",
+          col=colorRB1,
+          trace="none",colsep=(1:(ncol(x))),rowsep=(1:(nrow(x))),sepcolor="black",
+          srtCol=60,cexCol=0.8,cexRow=0.8,sepwidth=c(0.07,0.07),
+          adjRow = c(0,0.5),
+          #labRow=c(rep("1 cell",50),data[51:77,2]),
+          #labCol=c(target_lab,target_lab),
+          breaks=seq(0,40,0.5),
+          lmat=rbind(c(0,3,3,2,0),
+                     c(0,1,1,0,0),
+                     c(0,1,1,0,0),
+                     c(4,4,4,0,0)),
+          lwid=c(0.5,0.8,0.8,3,0.05), lhei=c(0.25,0.7,0.7,0.5),
+          density.info="none",keysize=0.5)
+
+title("Single cell qPCR", cex.main = 2)
+
+#Save 5000x35000
+
